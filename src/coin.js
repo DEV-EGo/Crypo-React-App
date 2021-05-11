4af09d13f14aeb2c20f78a040fa5ab2e44ea4c66
Edwin <dev.edwing@gmail.com>
2021-05-10 21:59:06 -0400

    jsx

---
+++ src/coin.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const coin = () => {
+    return (
+        <div className = 'coin-container'>
+            <div className = 'coin-row'>
+                <div className = 'coin'>
+                    <img src = {image} alt = 'crypto'></img>
+                    <h1> {name}</h1>
+                    <p className = 'coin-symbol'>{Symbol} </p>
+                </div>
+
+                <div className = 'coin-data'>
+                    <p className = 'coin-price'> {price}</p>
+                    <p className = 'coin-volume'> {volume.toLocaleString()}</p>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default coin
